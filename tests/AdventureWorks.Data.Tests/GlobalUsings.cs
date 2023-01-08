86a382c49286a03488a180c629dbb8d2f4a6c16e
Mick Letofsky <mick.tosk@gmail.com>
2023-01-08 11:30:55 -0700

    Moving unit tests project around

--- tests/AdventureWorks.Data.Tests/GlobalUsings.cs
+++
@@ -1,6 +0,0 @@
-﻿global using FluentAssertions;
-global using FluentAssertions.Execution;
-global using Moq;
-global using System.Diagnostics.CodeAnalysis;
-global using AdventureWorks.Test.Common.Setup;
-global using Xunit;
---
+++ tests/AdventureWorks.UnitTests/GlobalUsings.cs
@@ -0,0 +1,6 @@
+﻿global using FluentAssertions;
+global using FluentAssertions.Execution;
+global using Moq;
+global using System.Diagnostics.CodeAnalysis;
+global using AdventureWorks.Test.Common.Setup;
+global using Xunit;
