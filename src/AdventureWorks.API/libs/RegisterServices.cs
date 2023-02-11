ef29dd5c7f7eba3f18e9b6c8679b72ac5f4ae715
Mick Letofsky <mick.tosk@gmail.com>
2023-02-11 09:32:09 -0700

    Rework & setup app config for JWT

--- src/AdventureWorks.API/libs/RegisterServices.cs
+++ src/AdventureWorks.API/libs/RegisterServices.cs
@@ -9,6 +9,7 @@ using AdventureWorks.Infrastructure.Persistence.DbContexts;
 using FluentValidation;
 using Microsoft.AspNetCore.Mvc.Controllers;
 using Microsoft.AspNetCore.Mvc.Versioning;
+using Microsoft.CodeAnalysis.Options;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.OpenApi.Models;
 using Swashbuckle.AspNetCore.SwaggerUI;
@@ -50,14 +51,39 @@ internal static class RegisterServices
 
         builder.Services.AddEndpointsApiExplorer();
 
-        builder.Services.AddSwaggerGen(c =>
+        builder.Services.AddSwaggerGen(options =>
         {
-            c.SwaggerDoc(
+            options.SwaggerDoc(
                 "v1",
                 MakeOpenApiInfo("Adventure Works API",
                                 "v1",
                                 "API",
                                 new Uri("http://hello-world.info")));
+
+            options.AddSecurityDefinition("Bearer", new OpenApiSecurityScheme
+            {
+                In = ParameterLocation.Header,
+                Description = "Please enter a valid token",
+                Name = "Authorization",
+                Type = SecuritySchemeType.Http,
+                BearerFormat = "JWT",
+                Scheme = "Bearer"
+            });
+
+            options.AddSecurityRequirement(new OpenApiSecurityRequirement
+            {
+                {
+                    new OpenApiSecurityScheme
+                    {
+                        Reference = new OpenApiReference
+                        {
+                            Type=ReferenceType.SecurityScheme,
+                            Id="Bearer"
+                        }
+                    },
+                    Array.Empty<string>()
+                }
+            });
 
             var xmlFile = $"{Assembly.GetExecutingAssembly().GetName().Name}.xml";
 
@@ -65,10 +91,10 @@ internal static class RegisterServices
 
             if (File.Exists(xmlPath))
             {
-                c.IncludeXmlComments(xmlPath);
+                options.IncludeXmlComments(xmlPath);
             }
 
-            c.TagActionsBy(api =>
+            options.TagActionsBy(api =>
             {
                 if (api.GroupName != null)
                 {
@@ -83,7 +109,7 @@ internal static class RegisterServices
                 throw new InvalidOperationException("Unable to determine tag for endpoint.");
             });
 
-            c.DocInclusionPredicate((name, api) => true);
+            options.DocInclusionPredicate((name, api) => true);
         });
 
         builder.Services.AddAutoMapper(
