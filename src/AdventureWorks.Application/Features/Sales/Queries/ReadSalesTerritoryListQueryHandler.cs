c0bfc77a1bf15ff5ffc049749d510d327d98142f
Mick Letofsky <mickl@knowledgeservices.com>
2025-04-15 10:19:05 -0600

    Refactor Sales Territory

---
+++ src/AdventureWorks.Application/Features/Sales/Queries/ReadSalesTerritoryListQueryHandler.cs
@@ -0,0 +1,21 @@
+﻿using AdventureWorks.Application.PersistenceContracts.Repositories;
+using AdventureWorks.Models.Features.Sales;
+using AutoMapper;
+using MediatR;
+
+namespace AdventureWorks.Application.Features.Sales.Queries;
+
+public sealed class ReadSalesTerritoryListQueryHandler(
+    IMapper mapper,
+    ISalesTerritoryRepository salesTerritoryRepository)
+    : IRequestHandler<ReadSalesTerritoryListQuery, List<SalesTerritoryModel>>
+{
+    private readonly IMapper _mapper = mapper ?? throw new ArgumentNullException(nameof(mapper));
+    private readonly ISalesTerritoryRepository _repository = salesTerritoryRepository ?? throw new ArgumentNullException(nameof(salesTerritoryRepository));
+
+    public async Task<List<SalesTerritoryModel>> Handle(ReadSalesTerritoryListQuery request, CancellationToken cancellationToken)
+    {
+        var entities = await _repository.ListAllAsync();
+        return entities is not { Count: > 0 } ? [] : _mapper.Map<List<SalesTerritoryModel>>(entities);
+    }
+}
